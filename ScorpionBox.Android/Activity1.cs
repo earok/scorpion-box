c078aa75104816ce960f221331556bdf7618b627
Erik Hogan <earok3d@gmail.com>
2026-02-02 09:01:01 +1300

    Added stand-in Android application (not tested, no cores)

---
+++ ScorpionBox.Android/Activity1.cs
@@ -0,0 +1,34 @@
+﻿using ScorpionBox.Core;
+using SK.Libretro.Utilities;
+
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using Microsoft.Xna.Framework;
+
+namespace ScorpionBox.Android;
+[Activity(
+    Label = "@string/app_name",
+    MainLauncher = true,
+    Icon = "@drawable/icon",
+    AlwaysRetainTaskState = true,
+    LaunchMode = LaunchMode.SingleInstance,
+    ScreenOrientation = ScreenOrientation.FullUser,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize
+)]
+public class Activity1 : AndroidGameActivity
+{
+    private View _view;
+
+    protected override void OnCreate(Bundle bundle)
+    {
+        base.OnCreate(bundle);
+
+        using var game = new ScorpionBoxGame(new DllModuleAndroid(), "so");
+        _view = game.Services.GetService(typeof(View)) as View;
+
+        SetContentView(_view);
+        game.Run();
+    }
+}
