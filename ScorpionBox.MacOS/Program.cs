f7701a8e09c66ea5f15835294b16c1af345af5cf
Erik Hogan <earok3d@gmail.com>
2026-02-02 10:26:03 +1300

    DYLIB fix

--- ScorpionBox.MacOS/Program.cs
+++ ScorpionBox.MacOS/Program.cs
@@ -1,5 +1,5 @@
 ﻿using ScorpionBox.Core;
 using SK.Libretro.Utilities;
 
-using var game = new ScorpionBoxGame(new DllModuleMacOS(), "dylib");
+using var game = new ScorpionBoxGame(new DllModuleMacOS(), ".dylib");
 game.Run();
